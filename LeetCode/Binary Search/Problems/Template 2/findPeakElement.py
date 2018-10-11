a9edd07419ad092251e53fee98a54b076fbbb85e
AWright2018 <adomous.wright@uconn.edu>
2018-10-10 23:41:32 -0400

    Started LC - Find Peak Element

---
+++ LeetCode/Binary Search/Problems/Template 2/findPeakElement.py
@@ -0,0 +1,39 @@
+"""
+Problem Description:
+    A peak element is an element that is greater than its neighbors. Given an
+    input array nums, where nums[i] =/= nums[i+1], find a peak element and
+    return its index. The array may contain multiple peaks, in that case return
+    the index to any one of the peaks is fine.
+
+Example
+Input:
+nums = [1,2,3,1]
+
+Output:
+2
+"""
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Initialize right and left
+        l = 0
+        r = len(nums)
+
+        # Binary Search
+        while (l < r):
+            m = (l + r) // 2
+
+            if (nums[m] > nums[m-1] and nums[m] > nums[m+1]):
+                return m
+            elif (nums[m] < nums[m-1]):
+                r = m
+            elif (nums[m] < nums[m+1]):
+                l = m + 1
+
+        if (l != len(nums)):
+            if (nums[m] > nums[m-1] and nums[m] > nums[m+1]):
+                return m
+        return -1
