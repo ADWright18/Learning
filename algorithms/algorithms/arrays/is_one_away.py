3868df4f4008d2a948bdddd547afef24e9b5d978
Adomous Wright <adomous.wright@uconn.edu>
2018-11-07 09:00:37 -0500

    Completed is_one_away for arrays

--- algorithms/algorithms/arrays/is_one_away.py
+++ algorithms/algorithms/arrays/is_one_away.py
@@ -3,8 +3,8 @@ def is_one_away(s1, s2):
     Checks if s1 and s2 are one edit distance away.
 
     Converts the given strings into a list of characters
-    and checks if the difference between the two lists
-    is at most 1
+    and checks if the intersection of s1 and s2 are one
+    away from the length of both s1 and s2
 
     Parameters
     ----------
@@ -19,4 +19,28 @@ def is_one_away(s1, s2):
         If the edit distance is at most 1, return True.
         Otherwise, return False
     """
-    
+    # Create a list of characters for each string
+    a = list(s1)
+    b = list(s2)
+    c = a + b
+
+    # Store the intersection of a and b
+    d = []
+
+    # Combine the lists
+    # If a character is in both a and b, remove that character from a and b
+    for i in c:
+        if (i in a and i in b):
+            d.append(i)
+            a.remove(i)
+            b.remove(i)
+
+    # Check if the length of s1 and s2 are at most one away from the
+    # length of the intersection
+    if (len(s1) > len(d) + 1 or len(s1) < len(d) - 1):
+        return False
+
+    if (len(s2) > len(d) + 1 or len(s2) < len(d) - 1):
+        return False
+
+    return True
