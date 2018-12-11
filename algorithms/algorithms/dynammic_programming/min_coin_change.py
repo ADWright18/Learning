c80f6f2dae22bf0ddcc9375abf6ee7d771dca7dd
Adomous Wright <adomous.wright@uconn.edu>
2018-12-11 08:33:15 -0500

    Completed Minimum Coin Change

--- algorithms/algorithms/dynammic_programming/min_coin_change.py
+++ algorithms/algorithms/dynammic_programming/min_coin_change.py
@@ -17,23 +17,30 @@ def min_coin_change(coins, target):
         If the target value can be reached with the given
         coin denominations, return the minimum number of
         coins to reach the target. Otherwise, return -1
-    
+
     """
+    # Initialize the table
+    table = [0 for i in range(target + 1)]
+
     # Base Case
-    if (target == 0):
-        return 0
+    table[0] = 0
 
     # Initialize result
-    result = sys.maxsize
+    for i in range(1, len(table)):
+        table[i] = sys.maxsize
 
-    # Try every coin that has smaller value than target
-    for i in range(0, len(coins)):
-        if (coins[i] <= target):
-            sub_result = min_coin_change(coins, target - coins[i])
+    # Bottom-up DP
+    for i in range(1, target + 1):
+        for j in range(0, len(coins)):
+            if (coins[j] <= i):
+                sub_result = table[i - coins[j]]
 
             # Check for INT_MAX to avoid overflow and see if
             # results can be minimized
-            if (sub_result != sys.maxsize and sub_result + 1 < result):
-                result = sub_result + 1
+                if (sub_result != sys.maxsize and sub_result + 1 < table[i]):
+                    table[i] = sub_result + 1
 
-    return result
+    if (table[target] > target):
+        return -1
+    else:
+        return table[target]
